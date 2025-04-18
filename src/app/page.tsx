f08b559d4f4b546e200133e52555b6608e1f5093
djvamps <dalopeza9@hotmail.com>
2025-04-17 19:48:20 -0500

    Actualizacion de imagen de video a webp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,10 @@ import WhatsAppButton from "@/components/WhatsAppButton";
 
 export const metadata: Metadata = {
   title: "Pengrafic - Agencia de Marketing",
-  description: "Estas en Internt luego existes, somos una agencia integral creativa. Realizamos proyectos de imagen, diseño, marketing digital, audiovisuales.",
+  description: "Estas en Internt luego existes, somos una agencia integral creativa. Realizamos proyectos de imagen, diseño, desarrollo web, aplicasiones moviles.",
   
   other: {
-    keywords: "marketing digital, agencia de marketing, diseño gráfico, audiovisuales, branding, publicidad, redes sociales, SEO, contenido digital", // Agrega tus keywords aquí
+    keywords: "marketing digital, diseño gráfico, audiovisuales, web, ecommerce, publicidad, redes sociales, SEO, contenido digital", // Agrega tus keywords aquí
   },
 
   // other metadata
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -12,10 +12,10 @@ const Hero = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                Impulsa tu negocio al siguiente nivel con un eCommerce y Landing Page a tu medida
+                Potencia tu negocio con un eCommerce o Landing Page diseñados para vender
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                Diseñamos plataformas personalizadas que atraen, convierten y venden, optimizando cada clic para maximizar tus resultados. ¡Transforma visitantes en clientes hoy!
+                Atrae más clientes, aumenta tus ventas y optimiza cada clic con una plataforma hecha a tu medida. ¡Convierte visitantes en ventas hoy mismo!
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
